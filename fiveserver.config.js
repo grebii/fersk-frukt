a2e18144810c83a18300d4d59c965a94a7a46a6b
atornity <102953017+grebii@users.noreply.github.com>
2022-04-07 20:15:09 +0200

    v

---
+++ fiveserver.config.js
@@ -0,0 +1,10 @@
+// fiveserver.config.js
+module.exports = {
+    highlight: true, // enable highlight feature
+    injectBody: true, // enable instant update
+    injectCss: true, // disable injecting css
+    navigate: true, // enable auto-navigation
+
+    /** This option lets the browser open with your local IP. */
+    useLocalIp: true
+};
